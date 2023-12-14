b6e73011a753801bb0298615c686c105dd8c304c
carolrochafloro <carolinarochaf@hotmail.com>
2023-12-14 10:13:19 -0300

    <refactor> Index.js dos middlewares

---
+++ src/middleware/index.js
@@ -0,0 +1,5 @@
+const checkInfoNewUser = require("./checkInfoNewUser");
+const checkEmailExists = require("./checkEmailExists");
+const checkAuth = require("./checkAuth");
+
+module.exports = { checkAuth, checkEmailExists, checkInfoNewUser };
